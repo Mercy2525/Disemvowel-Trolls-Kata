582601888c5af5b78c44a47a3bd504c6a7110320
ASUS <you@example.com>
2023-08-25 17:30:00 +0300

    added comments

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+//SOLUTION 1
 
+function disemvowel(str) {
+     // Array of vowels in both lowercase and uppercase.
+ vowels=['a','e','i','o','u','A','E','I','O','U']
 
+    // Split the input string into an array of characters.
+const splittedStr=str.split("")
 
-// function disemvowel(str) {
-//  vowels=['a','e','i','o','u','A','E','I','O','U']
-   
-//   const splittedStr=str.split("")
-//   const filteredStr= splittedStr.filter((character)=>{
-//     return !vowels.includes(character)
-    
-//   })
+    // Filter out characters that are vowels using the filter() method.
+const filteredStr= splittedStr.filter((character)=>{
+    return !vowels.includes(character)
+  })
   
-// return filteredStr.join('')
+  // Join the filtered characters back together into a string and return it.
+return filteredStr.join('')
+}
 
-// }
+
+// SOLUTION 2
+
+// The replace() method replaces all vowels with an empty string (''), removing them.
+// This returns a new string with vowels removed.
 
 function disemvowel(str) {
   // Use regular expression to remove vowels (case-insensitive)
